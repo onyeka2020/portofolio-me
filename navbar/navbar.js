ce64ce48d8714eac9eb7313324810fc3ee4dd36a
onyeka2020 <kah.kanye@gmail.com>
2020-12-07 14:40:31 +0100

    i just comment

---
+++ navbar/navbar.js
@@ -0,0 +1,21 @@
+// function navy() {
+//     var x = document.getElementById('nav-mobo').style.display="block";
+    
+// }
+
+// function navi() {
+//   var x = document.getElementById('nav-mobo').style.display="hide";
+  
+// }
+
+
+
+
+function navy() {
+    var x = document.getElementById("nav-mobo").classList.toggle("shownav");
+   
+  }
+
+
+
+
